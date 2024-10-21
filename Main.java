247b966b462d05eac7711ce068f23909a614a7b3
Victoriashulman <victorias252@nycstudents.net>
2024-10-21 15:49:26 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,1 +1,14 @@
+import java.util.ArrayList;
+
+public static void main(String[] args)
+    {
+        WordChecker w = new WordChecker();
+           ArrayList<String> words = new ArrayList<String>();{
+           words.add("an");
+           words.add("band");
+           words.add("band");
+           words.add("abandon");
+           }
+
+    }
 
